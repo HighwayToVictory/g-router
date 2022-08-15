06fe4b94022386a4afad7e7b4abdb068f17c4b83
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-15 19:07:04 +0430

    add: prometheus

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,1 +1,13 @@
 package metrics
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func NewMetrics() {
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+	}()
+}
