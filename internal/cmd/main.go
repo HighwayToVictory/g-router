fcbf988556a1953a541046d0ce1edfd0ee1bc26c
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-18 12:24:13 +0430

    update: main

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/HighwayToVictory/g-router/internal/configs"
 	"github.com/HighwayToVictory/g-router/internal/metrics"
@@ -12,9 +17,39 @@ func Execute() {
 	wg := sync.WaitGroup{}
 	cfg := configs.Load()
 
-	rt := router.New(cfg.Router)
+	go putOnRouter(cfg.Router)
 
 	metrics.Register(&wg)
 
 	wg.Wait()
+}
+
+func randomIp() string {
+	min := 0
+	max := 255
+
+	var parts []int
+
+	for i := 0; i < 4; i++ {
+		parts = append(parts, rand.Intn(max-min)+min)
+	}
+
+	return strings.Trim(strings.Replace(fmt.Sprint(parts), " ", ".", -1), "[]")
+}
+
+func putOnRouter(cfg router.Config) {
+	// register
+	rt := router.New(cfg)
+
+	for i := 1; i < cfg.NumberOfInterface; i++ {
+		rt.RegisterIp(strconv.Itoa(i), randomIp())
+	}
+
+	for {
+		ip := randomIp()
+
+		rt.Next(ip)
+
+		time.Sleep(1 * time.Second)
+	}
 }
