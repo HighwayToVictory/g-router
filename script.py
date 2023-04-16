fb0fe6aa740da42918884c45dc4d9e298e21b7ee
amirhnajafiz <najafizadeh21@gmail.com>
2023-04-16 20:43:00 +0330

    add: enable linux iproute

--- script.py
+++ script.py
@@ -3,3 +3,17 @@ import argparse
 import time
 import os
 import sys
+
+
+
+def _enable_linux_iproute():
+    """
+    Enables IP route ( IP Forward ) in linux-based distros.
+    """
+    file_path = "/proc/sys/net/ipv4/ip_forward"
+    
+    with open(file_path) as f:
+        if f.read() == 1:
+            return  # already enabled
+    with open(file_path, "w") as f:
+        print(1, file=f)
