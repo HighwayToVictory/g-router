43828a8229bbd5e41f0c10c0d32164777ac898eb
amirhnajafiz <najafizadeh21@gmail.com>
2023-08-02 17:50:23 +0330

    init: home page

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>
+                Welcome Cavalier
+            </h1>
+            <p>
+                Cavalier is the Stallion cluster dashboard.
+            </p>
+        </div>
+    )
+}
